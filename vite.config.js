619813d05a92e7a45cb34533930ecc667c0abf79
CodeWithChirag14 <chiragnankani15@gmail.com>
2023-07-22 22:01:44 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ChiragRealEstate-React/",
+  base: "/RealEstate/",
   plugins: [react()],
 })
